71f95a885959029d34e5d9a2964863dd729def3c
Punyapat Chanthakhun <zunzuza212547@gmail.com>
2024-08-07 13:47:30 +0700

    adding a info on event page

--- config/site.ts
+++ config/site.ts
@@ -30,34 +30,6 @@ export const siteConfig = {
 			label: "Event",
 			href: "/event",
 		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
 	],
 	links: {
 		github: "https://github.com/zunzureal",
