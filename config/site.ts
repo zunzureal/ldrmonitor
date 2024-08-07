ca76d390c6d5a0f328ff96e0969c84cdf81e7ecd
Punyapat Chanthakhun <zunzuza212547@gmail.com>
2024-08-07 13:45:22 +0700

    adding a info on event page

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "zunzu",
-	description: "Just a daily of zunzu life.",
+	name: "Punyapat Chanthakhun",
+	description: "Welcome to my world.",
 	navItems: [
 		{
 			label: "Home",
